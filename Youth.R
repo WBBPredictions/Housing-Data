52912e1da3224d864bc9c697e4da40123e2624d5
madhumita152 <39741434+madhumita152@users.noreply.github.com>
2018-06-27 16:29:54 -0700

    Youth update
    Age of the house from year built / remodelled. Youth of  a house  that was never remodelled  is same as year built

--- Youth.R
+++ Youth.R
@@ -1,18 +1,26 @@
 #HOUSING DATA
 #AGE OF THE HOUSES 
-my.data.copy<-my.data
+
+my.data<-read.csv(file.choose())
+
 
 current.year<- 2018
 
 #Age from year built
-age.YBuilt<- current.year- (my.data.copy$YearBuilt)
+age.YBuilt<- current.year- (my.data$YearBuilt)
 
 #Age from year remodelled
-age.YRemod<- current.year- (my.data.copy$YearRemodAdd)
+age.YRemod<- current.year- (my.data$YearRemodAdd)
 
 #Adding the age columns in my.data.copy
-my.data.copy$Age.YearBuilt<- age.YBuilt
-my.data.copy$Age.YearRemod<- age.YRemod
+my.data$Age.YearBuilt<- age.YBuilt
+my.data$YearRemod<- age.YRemod
+
 #Should we filter the houses that were never remodelled and the onces that were
 #remodlled.  Also, should we consider that a remodelled a old house is 
 #equivalent to a newly contructed house? 
+if(age.YBuilt==age.YRemod){
+  youth<-age.YRemod
+} else{ youth<- age.YBuilt}
+
+my.data$Youth<-youth
